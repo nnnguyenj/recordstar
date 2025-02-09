7ea625427be8f04025799012a296cf51c2633f92
nnnguyenj <twq7um@virginia.edu>
2025-02-09 10:45:51 -0500

    views and test created

--- recordstar/tests.py
+++ recordstar/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# SHERRIFF: Dummy test added.
+class DummyTestCase(TestCase):
+    def setUp(self):
+        x = 1
+        y = 2
+    
+    def test_dummy_test_case(self):
+        self.assertEqual(1, 2)
--- recordstar/views.py
+++ recordstar/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# SHERRIFF: very basic index page created
+def index(request):
+    return HttpResponse("This now has a failed test.")
 # Create your views here.
