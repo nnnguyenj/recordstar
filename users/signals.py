ecd3bf1d7864a2df096e5f1457d89d6028502433
suhanimalhotra <suhani.malhotra@gmail.com>
2025-02-19 00:50:39 -0500

    updated the initial login page so that there is a dropdown that lets you choose between logging into a librarian or patron account

--- users/signals.py
+++ users/signals.py
@@ -5,4 +5,5 @@ from .models import Profile
 @receiver(user_signed_up)
 def create_profile(sender, request, user, **kwargs):
     # create Profile for each new user, default is patron
-    Profile.objects.create(user=user, account_type='P')
+    account_type = request.session.pop("account_type", "P")
+    Profile.objects.create(user=user, account_type=account_type) #fetches account_type from session, defaults to P if no type provided
--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-
+from django.urls import reverse
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.account.utils import perform_login
 
 # Create your views here.
 
 def index_view(request):
+    if request.method == "POST":
+        account_type = request.POST.get("account_type", "P") #defaults to patron
+        request.session["account_type"] = account_type #stores in session
+        return redirect(reverse("socialaccount_login", args=["google"])) #redirects to Google login
     return render(request, "users/index.html")
 
 def logout_view(request):
