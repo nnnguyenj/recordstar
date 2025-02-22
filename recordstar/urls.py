cb6b4e9dcc87028d71e19f4f8028d5bbb1060b1a
aqe6cc <aqe6cc@virginia.edu>
2025-02-21 21:16:12 -0500

    Current progress on allowing users to change profile pictures.

--- recordstar/urls.py
+++ recordstar/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Example URL pattern:
     # path('example/', views.example_view, name='example'),
     path("", views.IndexView.as_view(), name="index"),
     path("add_item/", views.add_item, name="add_item"),
-]
+    path("update_picture/", views.update_profile_picture, name='update_profile_picture'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recordstar/views.py
+++ recordstar/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 
 
 # SHERRIFF: very basic index page created
+from users.models import Profile
+
+
 class IndexView(generic.TemplateView):
     template_name = "recordstar/index.html"
 # Create your views here.
+
+@login_required
+def update_profile_picture(request):
+    if request.method == 'POST' and request.FILES['profile_picture']:
+        profile = Profile.objects.get(user=request.user)
+        profile.profile_picture = request.FILES['profile_picture']
+        profile.save()
+        return redirect(reverse("index"))
+    return render(request, "recordstar/upload_picture.html")  # Render a page with the upload form
 
 @login_required
 def add_item(request):
