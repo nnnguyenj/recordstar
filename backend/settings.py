e2f22abf55c05f1499d7ffe07fb983d555f28ffa
nnnguyenj <twq7um@virginia.edu>
2025-04-12 18:53:37 -0400

    making sure everything is working with images to heroku

--- backend/settings.py
+++ backend/settings.py
@@ -164,7 +164,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
